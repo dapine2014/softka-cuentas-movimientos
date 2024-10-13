f6b175ced495b034067560c0ca3a3c33472176d4
alex <dapine@gmail.com>
2024-10-13 16:19:03 -0500

    Initial commit

---
+++ src/main/java/servicio/softkacuentasmovimientos/SoftkaCuentasMovimientosApplication.java
@@ -0,0 +1,13 @@
+package servicio.softkacuentasmovimientos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftkaCuentasMovimientosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftkaCuentasMovimientosApplication.class, args);
+    }
+
+}
